39c6333862408258d9341b9b106783dfb6388fde
Vinay <vinayhg87@gmail.com>
2019-09-18 17:16:13 +0530

    only change in the input shock data

--- Serialization/NSEStockData.py
+++ Serialization/NSEStockData.py
@@ -6,7 +6,7 @@ class JsonParser:
     count = 0
     pyobject = JsonSerialization.StockData()
     for x in range(9, 20):
-        ltp = pyobject.fetchStockltp("TATASTEEL", str(x))
+        ltp = pyobject.fetchStockltp("VEDL", str(x))
         if ltp != 0:
             print(ltp)
             break
