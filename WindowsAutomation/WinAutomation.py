7ed6779c84536dbd9989d1be7703d0ff88ccf3c5
Vinay <vinayhg87@gmail.com>
2019-09-19 18:06:39 +0530

    No functionality changes

--- WindowsAutomation/WinAutomation.py
+++ WindowsAutomation/WinAutomation.py
@@ -8,13 +8,14 @@ import time
 
 driver = webdriver.Firefox()
 driver.get("https://www.seleniumhq.org/download/")
+driver.maximize_window()
 driver.find_element_by_xpath("//td[text()='Java']/..//*[contains(text(),'Download')]").click()
 time.sleep(1)
+# Handling windows based pop using pywinauto package.
 keyboard.send_keys("{TAB}")
 keyboard.send_keys("{TAB}")
 time.sleep(1)
 keyboard.send_keys("{TAB}")
 keyboard.send_keys("{TAB}")
 keyboard.send_keys("{ENTER}")
-
 driver.close()
